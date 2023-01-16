8168d63996385727d6dfad653019be9ed24b2ee3
Samuel Evangelista <61480137+sahev@users.noreply.github.com>
2023-01-15 23:01:11 -0300

    Develop (#3)
    * att
    
    * upd yml
    
    * upd
    
    * upd
    
    * last
    
    * upd

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -41,8 +41,8 @@ export default {
     data: () => ({
         user: {
             initials: 'SE',
-            fullName: 'tstsetset',
-            email: 'sara@email.com',
+            fullName: 'mudei o nome',
+            email: 'samu@el.com',
             image: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
             role: 'doctor'
         },
