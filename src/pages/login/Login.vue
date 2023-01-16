dfb5df890330e1f3cc1d9354371ab6c491b3fd19
sahev <s4mus.sh@gmail.com>
2023-01-15 22:45:30 -0300

    upd

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -41,7 +41,7 @@ export default {
     data: () => ({
         user: {
             initials: 'SE',
-            fullName: 'Samuel Evangelista',
+            fullName: 'mudei o nome',
             email: 'samu@el.com',
             image: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
             role: 'doctor'
