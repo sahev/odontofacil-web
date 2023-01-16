c51ed1659ce530400267dda2199bc633358bcd0f
sahev <s4mus.sh@gmail.com>
2023-01-15 21:38:52 -0300

    add workflow

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -41,7 +41,7 @@ export default {
     data: () => ({
         user: {
             initials: 'SE',
-            fullName: 'saaaaaamuellll evangelistaaaaaaaaa',
+            fullName: 'tstsetset',
             email: 'sara@email.com',
             image: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
             role: 'doctor'
